1d670a7f8896a478e48924548abab3b7ba700d7c
Emanuel <plus.emanuelmustea@gmail.com>
2021-04-09 15:53:01 +0300

    Added docker

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const io = require("socket.io-client");
+const SOCKET_ADDRESS = process.env.SOCKET_ADDRESS || "http://localhost:8080";
+const socket = io(SOCKET_ADDRESS);
 
-const socket = io("http://localhost:8080");
+socket.on("connection", () => {
+  console.log("Connected to ", SOCKET_ADDRESS);
+});
 
 socket.on("toggle", (data) => {
   console.log(data);
